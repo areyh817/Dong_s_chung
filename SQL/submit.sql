62024d03266623c44e88892765d888262bb19f8d
조혜라 <s2037@e-mirim.hs.kr>
2022-04-10 23:50:11 +0900

    user 테이블 구조 변경

--- SQL/submit.sql
+++ SQL/submit.sql
@@ -1,17 +1,22 @@
-mysql> create table submit(
-    -> id int primary key,
+mysql> create table user(
+    -> id int not null auto_increment primary key,
+    -> student_id int not null unique,
     -> name varchar(30) not null,
-    -> tell int not null,
-    -> motive varchar(255) not null
+    -> tell int,
+    -> ca varchar(50),
+    -> motive varchar(255)
     -> );
+Query OK, 0 rows affected (0.05 sec)
 
-mysql> desc submit;
-+--------+--------------+------+-----+---------+-------+
-| Field  | Type         | Null | Key | Default | Extra |
-+--------+--------------+------+-----+---------+-------+
-| id     | int          | NO   | PRI | NULL    |       |
-| name   | varchar(30)  | NO   |     | NULL    |       |
-| tell   | int          | NO   |     | NULL    |       |
-| motive | varchar(255) | NO   |     | NULL    |       |
-+--------+--------------+------+-----+---------+-------+
-4 rows in set (0.02 sec)
+mysql> desc user;
++------------+--------------+------+-----+---------+----------------+
+| Field      | Type         | Null | Key | Default | Extra          |
++------------+--------------+------+-----+---------+----------------+
+| id         | int          | NO   | PRI | NULL    | auto_increment |
+| student_id | int          | NO   | UNI | NULL    |                |
+| name       | varchar(30)  | NO   |     | NULL    |                |
+| tell       | int          | YES  |     | NULL    |                |
+| ca         | varchar(50)  | YES  |     | NULL    |                |
+| motive     | varchar(255) | YES  |     | NULL    |                |
++------------+--------------+------+-----+---------+----------------+
+6 rows in set (0.01 sec)
