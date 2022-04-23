2f592e5993a79a231718d548fa00a8715dd38cc9
[haezzang] <songna89@naver.com>
2022-04-23 16:39:12 +0900

    로그아웃 세션 설정

--- logout.php
+++ logout.php
@@ -1,7 +1,13 @@
-<?php
- session_start();
- session_destroy();
+<?
+session_start();
+
+session_unset(); // 세션제거
+
+session_destroy(); // 세션삭제
 ?>
+
+
+출처: https://doolyit.tistory.com/124 [동해둘리의 IT Study]
 <script>
     alert("로그아웃");
 </script>
